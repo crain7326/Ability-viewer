3378295ec4c9699fefbeb3a2e2c8aa2f57c7d3d6
crain <crain7326@naver.com>
2022-03-27 15:01:26 +0900

    [chore] toggle button 수정 및 글꼴 적용 class 명 변경

--- src/Page/ViewerAll.js
+++ src/Page/ViewerAll.js
@@ -4,12 +4,10 @@ import indexStore from "../store/indexStore";
 
 const ViewerAll = () => {
   const { optionStore } = indexStore();
-  console.log(`fontSize: ${optionStore.fontSize}`);
-  console.log(`lineHeigth: ${optionStore.lineHeigth}`);
-  console.log(`lineHeigth: ${optionStore.paragraphHeigth}`);
+
   return(
     <div className='bg-white'>
-      {optionStore.textBundle.map((text, index) => <p key={index} className={`${optionStore.fontSize} ${optionStore.lineHeigth} ${optionStore.paragraphHeigth}`}>{text}</p>)}
+      {optionStore.textBundle.map((text, index) => <p key={index} className={`${optionStore.fontSize} ${optionStore.lineHeigth} ${optionStore.paragraphHeigth} ${optionStore.fontFamily}`}>{text}</p>)}
     </div>
   )
 };
--- src/components/Button/Toggle.js
+++ src/components/Button/Toggle.js
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
 const Toggle = () => {
-  const [left, setLeft] = useState(26);
+  const [left, setLeft] = useState(4);
 
   function handleClick() {
-    if(left === 26) {
-      setLeft(4);
+    if(left === 4) {
+      setLeft(26);
       return;
     } 
-    setLeft(26);
+    setLeft(4);
   }
   return (
     <div className='btnToggle-wrap'
@@ -17,12 +17,12 @@ const Toggle = () => {
     >
       {/* 테두리 */}
       <div
-        className={`br-12 b-900 button-border ${left === 26 ? 'bg-white' : 'bg-900'}`}
+        className={`br-12 b-900 button-border ${left !== 26 ? 'bg-white' : 'bg-900'}`}
         style={{width: 50, height: 26}}>
       </div>
       {/* 동그라미 버튼 */}
       <div
-        className={`br-circle bc-500 button-circle ${left === 26 ? 'bg-500' : 'bg-50'}`}
+        className={`br-circle bc-500 button-circle ${left !== 26 ? 'bg-500' : 'bg-50'}`}
         style={{ position: 'absolute', top: 3, left: left, width: 20, height: 20, transition: '.2s'}}></div>
     </div>
     
