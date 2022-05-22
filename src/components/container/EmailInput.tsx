aece94ef04c140c8e86ff18cb4f08c1edb42a689
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-05-22 09:26:14 +0900

    [Feat] signupPage에 EmailLabelInput 분리

---
+++ src/components/container/EmailInput.tsx
@@ -0,0 +1,52 @@
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import LabelInput from "../common/LabelInput";
+
+export interface EmailInpurProps {
+  setValue: Dispatch<SetStateAction<string>>;
+}
+
+const EmailInput = ({ setValue }: EmailInpurProps) => {
+  const [emailList, setEmailList] = useState<string[]>([]);
+
+  const frequencyEmails = [
+    "@naver.com",
+    "@gmail.com",
+    "@daum.net",
+    "@hanmail.net",
+    "@yahoo.com",
+    "@outlook.com",
+    "@nate.com",
+    "@kakao.com",
+  ];
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    const userEmails = frequencyEmails.map((email) =>
+      e.target.value.includes("@")
+        ? e.target.value.split("@")[0] + email
+        : e.target.value + email
+    );
+    setEmailList(userEmails);
+    userEmails.map(
+      (email) => e.target.value == email && setValue(e.target.value)
+    );
+  };
+
+  return (
+    <>
+      <LabelInput
+        label="이메일"
+        type="email"
+        name="email"
+        list="email"
+        placeholder="이메일을 입력하세요"
+        onChange={onChangeEmail}
+      />
+      <datalist id="email">
+        {emailList &&
+          emailList.map((email, idx) => <option value={email} key={idx} />)}
+      </datalist>
+    </>
+  );
+};
+
+export default EmailInput;
