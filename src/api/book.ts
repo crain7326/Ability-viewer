16d20f5ab1cd8946cbbee17e933a2f1d47f8fcda
ixio0330 <ixio0330@gmail.com>
2022-07-03 14:07:14 +0900

    [Feat] book api 로직

---
+++ src/api/book.ts
@@ -0,0 +1,65 @@
+import api from './api';
+
+const bookApi = {
+  // 단일 책 조회
+  getBookById() {},
+  // 특정 회원의 모든 책 조회
+  getAllBooks() {},
+  // 특정 회원, 특정 태그의 모든 책 조회
+  getBookByTag() {},
+
+  // 책 저장
+  createBook(data: {
+    book: {
+      name: string;
+      text: string;
+    };
+    tags?: [
+      {
+        name: string;
+      }
+    ];
+  }) {
+    return api(
+      {
+        method: 'POST',
+        data,
+      },
+      `${process.env.REACT_APP_API_URL}/books`
+    );
+  },
+
+  // 책 수정
+  updateBook(
+    data: {
+      book: {
+        name: string;
+        text: string;
+      };
+      tags?: [
+        {
+          name: string;
+        }
+      ];
+    },
+    book_id: string
+  ) {
+    return api(
+      {
+        method: 'PATCH',
+        data,
+      },
+      `${process.env.REACT_APP_API_URL}/books/${book_id}`
+    );
+  },
+
+  // 책 삭제
+  deleteBook(book_id: string) {
+    return api(
+      {
+        method: 'DELETE',
+      },
+      `${process.env.REACT_APP_API_URL}/books/${book_id}`
+    );
+  },
+};
