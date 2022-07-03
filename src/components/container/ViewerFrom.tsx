a6214e29bba78897a30652b7d737780eb14beb80
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-07-03 14:44:16 +0900

    Fix: 전체보기 오류 수정, 저장하기 api 파라미터 오브젝트 제작

--- src/components/container/ViewerFrom.tsx
+++ src/components/container/ViewerFrom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import HashtagInput from './HashtagInput';
 
@@ -8,9 +8,27 @@ import Hashtag from '../common/Hashtag';
 const ViewerFrom = () => {
   // 내부 해시태그 string list object로 반환
 
-  const [bookName, setBookName] = useState<string>();
-  const [tags, setTags] = useState<{ name: string }[]>([]);
-  const [bookText, setBookText] = useState<string>();
+  const [bookName, setBookName] = useState<{ name: string }>();
+  const [bookTags, setBookTags] = useState<{ name: string }[]>([]);
+  const [bookText, setBookText] = useState<{ text: string }>();
+
+  const onChangeBookTitle = (e: ChangeEvent<HTMLInputElement>) =>
+    setBookName({ name: e.currentTarget.value });
+
+  const onChangeBookText = (e: ChangeEvent<HTMLInputElement>) =>
+    setBookText({ text: e.currentTarget.value });
+
+  const onClickViewAllBtn = () => {
+    optionStore.setText(bookText?.text);
+    optionStore.setTextBundle(optionStore.text);
+  };
+
+  const onClickSaveBtn = () => {
+    optionStore.setText(bookText?.text);
+    const book = { book: { ...bookName, ...bookText } };
+    const tags = { tags: [...bookTags] };
+    const requestObj = { ...book, ...tags };
+  };
 
   return (
     <>
@@ -18,18 +36,10 @@ const ViewerFrom = () => {
         className="ViewerBtn tc-500 mx-20 my-12"
         style={{ textAlign: 'right' }}
       >
-        <Link
-          to="/viewer_all"
-          onClick={() => {
-            optionStore.setText(
-              document.querySelector('.user_text')?.innerHTML
-            );
-            optionStore.setTextBundle(optionStore.text);
-          }}
-        >
+        <Link to="/viewer_all" onClick={onClickViewAllBtn}>
           전체보기
         </Link>
-        <Link to="/" className="ml-12">
+        <Link to="/" className="ml-12" onClick={onClickSaveBtn}>
           저장하기
         </Link>
       </div>
@@ -38,6 +48,7 @@ const ViewerFrom = () => {
           <input
             className="unset py-12"
             placeholder="제목을 입력하세요"
+            onChange={onChangeBookTitle}
             style={{ borderBottom: '1px solid var(--gray--300)' }}
           />
           <div
@@ -45,14 +56,15 @@ const ViewerFrom = () => {
             style={{ borderBottom: '1px solid var(--gray--300)' }}
           >
             <div className="hastagBox my-4 flex f-wrap">
-              {tags &&
-                tags.map((object, index) => (
+              {bookTags &&
+                bookTags.map((object, index) => (
                   <Hashtag key={index} text={object.name} />
                 ))}
             </div>
-            <HashtagInput setTags={setTags} tags={tags} />
+            <HashtagInput setTags={setBookTags} tags={bookTags} />
           </div>
           <textarea
+            onChange={onChangeBookText}
             className="unset border-box py-12 user_text"
             defaultValue={optionStore.text}
             style={{ height: 500 }}
