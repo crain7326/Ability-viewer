5c6bd658036f82299b55bf13ac4ffa094ce29130
seo <74411899+crain7326@users.noreply.github.com>
2022-07-10 15:48:11 +0900

    [Fix] 글꼴 선택 안되는 버그 수정 (#27)
    Co-authored-by: ixio0330 <ixio0330@gmail.com>

--- src/components/Button/ViewerType.tsx
+++ src/components/Button/ViewerType.tsx
@@ -1,22 +1,25 @@
-import indexStore from "../../store/indexStore";
+import indexStore from '../../store/indexStore';
+import { ViewType } from '../../store/optionStore';
 
-export interface ViewerTypeProps  {
-	type: string,
-  isSelected : boolean
-}	
+export interface ViewerTypeProps {
+  type: ViewType;
+  isSelected: boolean;
+}
 
-const ViewerType = ({ type, isSelected } : ViewerTypeProps) => {
+const ViewerType = ({ type, isSelected }: ViewerTypeProps) => {
   const { optionStore } = indexStore();
 
   return (
     <button
-      className={`unset br-8 px-8 py-4 ml-8 ${isSelected ? 'b-800 bg-800 tc-50' : 'b-800 bg-white tc-900'}`}
-      style={{cursor: 'pointer'}}
-      onClick={()=>optionStore.setViewerType(type === optionStore.viewer.ridi ? '리디북스' : '카카오')}
+      className={`unset br-8 px-8 py-4 ml-8 ${
+        isSelected ? 'b-800 bg-800 tc-50' : 'b-800 bg-white tc-900'
+      }`}
+      style={{ cursor: 'pointer' }}
+      onClick={() => optionStore.setViewerType(type)}
     >
       {type}
     </button>
-  )
+  );
 };
 
-export default ViewerType
+export default ViewerType;
