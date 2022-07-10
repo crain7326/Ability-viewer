18ea725fc1fb2404be859e7e150ca2a4205b8ee1
yuseongLee <82145837+meteor-or@users.noreply.github.com>
2022-07-10 16:00:41 +0900

    [Fix] view all 페이지 배경 오류 (#28)
    * Chore: 안 쓰는 설정 삭제
    
    * Fix: 백그라운드가 다 채워지지 않는 오류 수정

--- src/Page/ViewerAll.tsx
+++ src/Page/ViewerAll.tsx
@@ -1,15 +1,20 @@
-import indexStore from "../store/indexStore";
-
-
+import indexStore from '../store/indexStore';
 
 const ViewerAll = () => {
   const { optionStore } = indexStore();
 
-  return(
-    <div className='bg-white'>
-      {optionStore.textBundle.map((text, index) => <p key={index} className={`${optionStore.fontSize} ${optionStore.lineHeigth} ${optionStore.paragraphHeigth} ${optionStore.fontFamily}`}>{text}</p>)}
+  return (
+    <div className="bg-white h-full pt-24">
+      {optionStore.textBundle.map((text, index) => (
+        <p
+          key={index}
+          className={`${optionStore.fontSize} ${optionStore.lineHeigth} ${optionStore.paragraphHeigth} ${optionStore.fontFamily}`}
+        >
+          {text}
+        </p>
+      ))}
     </div>
-  )
+  );
 };
 
 export default ViewerAll;
