e1a15958a3cf514c772b803d249cf907ecdfaca5
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-07-20 13:52:59 +0900

    Refactoring: List 페이지에 하드코딩된 로직 수정

--- src/api/book.ts
+++ src/api/book.ts
@@ -1,75 +1,82 @@
 import { api, apiWithToken } from './api';
-import userStorage from '../helper/localStorage';
 import { Response } from './api';
 import { BookDto, BookDetailDto } from './book.dto';
 
 const bookApi = {
-  // 단일 책 조회
-  async getBookById(book_id: string): Promise<Response<BookDetailDto>> {
-    return await apiWithToken({
-      method: 'GET',
-      url: `${process.env.REACT_APP_API_URL}/books/${book_id}`,
-    });
-  },
-  // 특정 회원의 모든 책 조회
-  async getAllBooks(): Promise<Response<BookDto>> {
-    return await apiWithToken({
-      method: 'GET',
-      url: `${process.env.REACT_APP_API_URL}/books`,
-    });
-  },
-  // 특정 회원, 특정 태그의 모든 책 조회
-  getBookByTag() {
-    return apiWithToken({
-      method: 'GET',
-      url: `${process.env.REACT_APP_API_URL}/tags`,
-    });
-  },
-
-  // 책 저장
-  createBook(data: {
-    book: {
-      name: string;
-      text: string;
-    };
-    tags?: { name: string }[];
-  }) {
-    return apiWithToken({
-      method: 'POST',
-      url: `${process.env.REACT_APP_API_URL}/books`,
-      data,
-    });
-  },
-
-  // 책 수정
-  updateBook(
-    data: {
-      book: {
-        name: string;
-        text: string;
-      };
-      tags?: [
-        {
-          name: string;
-        }
-      ];
+    // 단일 책 조회
+    async getBookById(book_id: string): Promise<Response<BookDetailDto>> {
+        return await apiWithToken({
+            method: 'GET',
+            url: `${process.env.REACT_APP_API_URL}/books/${book_id}`,
+        });
     },
-    url: string
-  ) {
-    return apiWithToken({
-      method: 'PATCH',
-      data,
-      url,
-    });
-  },
+    // 모든 책 조회: 특정 회원
+    async getAllBooks(): Promise<Response<BookDto>> {
+        return await apiWithToken({
+            method: 'GET',
+            url: `${process.env.REACT_APP_API_URL}/books`,
+        });
+    },
 
-  // 책 삭제
-  deleteBook(url: string) {
-    return apiWithToken({
-      method: 'DELETE',
-      url,
-    });
-  },
+    // 태그 조회: 특정 회원, 모든 책
+    getTagByBook() {
+        return apiWithToken({
+            method: 'GET',
+            url: `${process.env.REACT_APP_API_URL}/tags`,
+        });
+    },
+    // 모든 책 조회: 특정 회원, 특정 태그
+    getBookByTag(url: string) {
+        return apiWithToken({
+            method: 'GET',
+            url,
+        });
+    },
+
+    // 책 저장
+    createBook(data: {
+        book: {
+            name: string;
+            text: string;
+        };
+        tags?: { name: string }[];
+    }) {
+        return apiWithToken({
+            method: 'POST',
+            url: `${process.env.REACT_APP_API_URL}/books`,
+            data,
+        });
+    },
+
+    // 책 수정
+    updateBook(
+        data: {
+            book: {
+                name: string;
+                text: string;
+            };
+            tags?: [
+                {
+                    name: string;
+                }
+            ];
+        },
+        url: string
+    ) {
+        return apiWithToken({
+            method: 'PATCH',
+            data,
+            url,
+        });
+    },
+
+    // 책 삭제
+    deleteBook(url: string) {
+        return apiWithToken({
+            method: 'DELETE',
+            url,
+        });
+    },
 };
 
 export default bookApi;
