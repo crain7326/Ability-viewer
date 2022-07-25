b3de4789672a2a82e0111a53b093eea956a0ebb2
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-07-25 09:05:39 +0900

    Chore: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+
 import Header from './components/base/Header';
+import ErrorBoundary from './components/error/ErrorBoundary';
+import Spinner from './components/common/Spinner';
+
 import ListPage from './page/ListPage';
 import LoginPage from './page/LoginPage';
 import Mypage from './page/Mypage';
@@ -8,13 +14,10 @@ import ViewerPage from './page/ViewerPage';
 import ViewerAll from './page/ViewerAll';
 import FindUserPage from './page/FindUserPage';
 import NotFound from './page/NotFound';
-import ErrorBoundary from './components/error/ErrorBoundary';
-import { useEffect, useState } from 'react';
+import BookDetail from "./page/BookDetail";
+
 import storage from './helper/localStorage';
-import BookDetail from './page/BookDetail';
-import Spinner from './components/common/Spinner';
 import indexStore from './store/indexStore';
-import { observer } from 'mobx-react-lite';
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
@@ -26,23 +29,23 @@ function App() {
 
   return (
     <ErrorBoundary>
-      <div className="App pretendard">
+      <div className='App pretendard'>
         <Header isLogin={isLogin} setIsLogin={setIsLogin} />
         <Spinner loading={appStore.loading} />
-        <main className="h-main bg-100">
+        <main className='h-main bg-100'>
           <Routes>
-            <Route path="/" element={<ViewerPage />} />
-            <Route path="/book/:id" element={<BookDetail />} />
-            <Route path="/viewer_all" element={<ViewerAll />} />
-            <Route path="/list" element={<ListPage />} />
-            <Route path="/mypage" element={<Mypage />} />
+            <Route path='/' element={<ViewerPage />} />
+            <Route path='/book/:id' element={<BookDetail />} />
+            <Route path='/viewer_all' element={<ViewerAll />} />
+            <Route path='/list' element={<ListPage />} />
+            <Route path='/mypage' element={<Mypage />} />
             <Route
-              path="/login"
+              path='/login'
               element={<LoginPage setIsLogin={setIsLogin} />}
             />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/find_user" element={<FindUserPage />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/find_user' element={<FindUserPage />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </main>
       </div>
